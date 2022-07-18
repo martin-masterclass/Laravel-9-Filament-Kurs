efeb5651945c1c5432e7243aee47457adfe205d5
Martin Krebs Eberth <>
2022-07-18 20:27:36 +0200

    Filament installiert & konfiguriert

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'name' => 'Martin',
+             'email' => 'martin@laravel-php.com',
+             'password' => bcrypt('bla123')
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(10)->create();
+
     }
 }
