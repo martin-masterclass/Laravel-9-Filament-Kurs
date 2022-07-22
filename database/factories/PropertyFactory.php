c28c72f51a2635dde7bbcfae63984888e8783291
Martin Krebs Eberth <>
2022-07-22 18:58:09 +0200

    Properties Factory

---
+++ database/factories/PropertyFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->realTextBetween(25,45),
+            'description' => $this->faker->realTextBetween(100,150),
+            'country' => $this->faker->country(),
+            'address' => $this->faker->address(),
+            'price' => rand(500, 5000)*1000,
+            'sqm' => rand(150, 1500),
+            'bedrooms' => rand(3, 10),
+            'bathrooms' => rand(3, 6),
+            'garages' => rand(1, 5),
+        ];
+    }
+}
