35a5f5ada55da27d107da707d4dec72f24526cd1
Martin Krebs Eberth <>
2022-07-26 17:58:36 +0200

    Media Library / fillable

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -5,15 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Property extends Model
+class Property extends Model implements HasMedia
 {
-    use HasFactory,SoftDeletes;
+    use HasFactory,SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
         'title',
         'description',
         'password',
+        'city',
         'country',
         'address',
         'price',
@@ -21,7 +24,8 @@ class Property extends Model
         'bedrooms',
         'bathroom',
         'garages',
-        'slider'
+        'slider',
+        'visible'
     ];
 
 }
