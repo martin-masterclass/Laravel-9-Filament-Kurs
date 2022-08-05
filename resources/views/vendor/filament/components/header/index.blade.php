f77c0abf85b3aaeb54559e86846fc42f918235be
Martin Krebs Eberth <>
2022-08-05 09:51:18 +0200

    Nachtrag Logo-Positionierung

--- resources/views/vendor/filament/components/header/index.blade.php
+++ resources/views/vendor/filament/components/header/index.blade.php
@@ -6,8 +6,8 @@
 <header {{ $attributes->class('space-y-2 items-start justify-between sm:flex sm:space-y-0 sm:space-x-4  sm:rtl:space-x-reverse sm:py-4 filament-header') }}>
     <x-filament::header.heading>
 
-        <div class="flex">
-            <div><img class="border mr-2" src="/logo.jpg" alt="Logo" style="width: 4.5rem;"></div>
+        <div class="flex items-center">
+            <div><img class="border mr-2 w-20" src="/logo.jpg" alt="Logo"></div>
             <div>{{ $heading }}</div>
         </div>
 
